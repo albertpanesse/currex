7328c2b28fe5c113b9ca278c08b24829a2a84989
Albert Panesse <albert.panesse@gmail.com>
2022-06-10 00:29:12 +0700

    done again #1

---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+export const currencies: NameSymbol[] = [
+  { name: 'United States Dollar', symbol: 'USD' },
+  { name: 'Canadian Dollar', symbol: 'CAD' },
+  { name: 'Indonesian Rupiah', symbol: 'IDR' },
+  { name: 'British Pound Sterling', symbol: 'GBP' },
+  { name: 'Swiss Franc', symbol: 'CHF' },
+  { name: 'Singapore Dollar', symbol: 'SGD' },
+  { name: 'Indian Rupee', symbol: 'INR' },
+  { name: 'Malaysian Ringgit', symbol: 'MYR' },
+  { name: 'Japanese Yen', symbol: 'JPY' },
+  { name: 'South Korean Won', symbol: 'KRW' },
+];
--- src/pages/Exchange/Exchange.tsx
+++ src/pages/Exchange/Exchange.tsx
@@ -1,21 +1,10 @@
 import React, { useState } from 'react';
 import objectHash from 'object-hash';
 
-import './style.scss';
 import { convert } from '../../apis';
+import { currencies } from '../../consts';
 
-const currencies: NameSymbol[] = [
-  { name: 'United States Dollar', symbol: 'USD' },
-  { name: 'Canadian Dollar', symbol: 'CAD' },
-  { name: 'Indonesian Rupiah', symbol: 'IDR' },
-  { name: 'British Pound Sterling', symbol: 'GBP' },
-  { name: 'Swiss Franc', symbol: 'CHF' },
-  { name: 'Singapore Dollar', symbol: 'SGD' },
-  { name: 'Indian Rupee', symbol: 'INR' },
-  { name: 'Malaysian Ringgit', symbol: 'MYR' },
-  { name: 'Japanese Yen', symbol: 'JPY' },
-  { name: 'South Korean Won', symbol: 'KRW' },
-];
+import './style.scss';
 
 export const Exchange = function () {
   const [isLoading, setIsLoading] = useState<boolean>(false);
