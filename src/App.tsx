ba23b8db4d70c149eb40c6b27cf4820c698120bc
Albert Panesse <albert.panesse@gmail.com>
2022-06-09 18:49:50 +0700

    first

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Exchange } from './pages';
+
+function App() {
+  return <Exchange />;
+}
+
+export default App;
---
+++ src/pages/Exchange/Exchange.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './style.scss';
+
+export const Exchange = function () {
+  return (
+    <div className='exchange'>
+      <div className='card'>
+        <div className='card-header'>Test</div>
+        <div className='card-body'>Doank</div>
+      </div>
+    </div>
+  );
+};
